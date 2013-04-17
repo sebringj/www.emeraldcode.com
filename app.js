0282f4ebf84936d3f12cdcc7ffb46173a89c8345
Jason Sebring <mail@jasonsebring.com>
2013-04-17 08:42:32 -0700

    fix in template bug

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express'),
 var app = express();
 
 app.configure(function(){
-	app.set('port', process.env.PORT || 3000);
 	app.engine('ejs', engine);
 	app.set('view engine', 'ejs');
 	app.set('views', __dirname + '/views');
